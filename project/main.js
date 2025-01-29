3e0a8a666f5e56f7e56ef82bf30bf9696cf2d80b
Aid Vllasaliu <aid@superkraft.io>
2025-01-29 12:41:46 +0100

    updated

--- project/main.js
+++ project/main.js
@@ -30,8 +30,14 @@ module.exports = class SK_App_Main {
             //minHeight: 200,
             //center: true,
             //alwaysOnTop: true,
+            //fullscreen: true
+            frame: false,
+            thickFrame: false
         })
 
+
+        var arrIdx = -1
+        var colors = ['darkcyan', '#60a23d', 'rgb(171 0 61)', 'hsl(271.35deg 100% 47.92%)']
         setInterval(() => {
             //var resizableVal = wnd.resizable
             //wnd.resizable = !resizableVal
@@ -41,10 +47,18 @@ module.exports = class SK_App_Main {
             //var val = wnd.isAlwaysOnTop()
             //wnd.setAlwaysOnTop(!val)
 
-            var attribute = 'movable'
+            
+            var attribute = 'thickFrame'
             var val = wnd[attribute]
             wnd[attribute] = !val
-        }, 1000)
+            wnd.title = attribute + ' = ' + !val
+            
+
+            /*arrIdx++
+            if (arrIdx >= colors.length) arrIdx = 0
+            wnd.backgroundColor = colors[arrIdx]
+            wnd.title = colors[arrIdx]*/
+        }, 3000)
     }
 
     postSKInit(sk) {
